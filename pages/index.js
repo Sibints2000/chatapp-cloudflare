89f5d205c624fe91f6a4c672712dadcf0e5ccf3a
Sibin Sajan <sibin.sajan@realiti.io>
2025-02-24 12:28:12 +0530

    Sibin || updated url

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   useEffect(() => {
     if (!isSignedIn || !user) return;
 
-    const ws = new WebSocket(process.env.NEXT_PUBLIC_CHAT_SERVER);
+    const ws = new WebSocket("wss://nextjs-chat-app.bornspy.workers.dev/ws");
     setSocket(ws);
 
     ws.onopen = () => {
